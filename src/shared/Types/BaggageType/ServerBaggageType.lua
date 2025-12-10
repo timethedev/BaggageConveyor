52b8ea2bcb203a53a178e9f8b49020ccb592e275
timethedev <timethedev@gmail.com>
2025-12-10 16:43:45 +0100

    Prototype

---
+++ src/shared/Types/BaggageType/ServerBaggageType.lua
@@ -0,0 +1,32 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Signal = require(ReplicatedStorage.Packages.Signal)
+local BaggageType = require(script.Parent)
+
+type Signal = typeof(Signal.Good.new())
+
+export type Data = {
+	Color: Color3?,
+	Material: Enum.Material?,
+	Speed: number?,
+	Start: Vector3,
+	Finish: Vector3,
+}
+
+export type Entity = {
+	Data: Data,
+	Color: Color3,
+	Material: Enum.Material,
+	Speed: number,
+	Destroyed: Signal,
+	__init: (self: Entity) -> Entity,
+	__setupData: (self: Entity) -> (),
+	__setupConnections: (self: Entity) -> (),
+	__replicate: (self: Entity) -> (),
+} & BaggageType.Entity
+
+export type Class = {
+	new: () -> Entity,
+	__index: Class,
+} & Entity & BaggageType.Class
+
+return {}
